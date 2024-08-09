59a922248ae7a863433c4b2ea54be373b8e791fe
JulianRengifo <jurenco1997@gmail.com>
2024-08-08 21:06:51 -0500

    Props
    En Header --> index.jsx usamos styled-components para los estilos de btn-primario y btn-secundario en la const BtnHeader
    
    Cambiamos la etiqueta a por BtnHeader
    
    Usamos props y operador ternario para diferenciar el color de cada boton
    
    En el boton Salir usamos la prop primary

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import logo from "../../assets/images/logo.svg";
+import { colorPrimario } from "../../UI/Variables";
 
 
 const StyledHeader = styled.nav`
@@ -16,18 +17,30 @@ const Logo = styled.img`
   height: 50px;
   width: 50px;
 `
+//usamos styled-components para los estilos de btn-primario y btn-secundario
+const BtnHeader= styled.a`
+  text-align: center;
+  border-radius: 3px;
+  padding: 5px 20px;
+  margin: 0 10px;
+  font-weight: 600;
+  border: 2px solid white;
+  //Usamos props y operador ternario para diferenciar el color de cada boton
+  color: ${(props) => props.primary ? "white": colorPrimario};
+  background: ${(props) => props.primary ? "transparent": "white"};
+`
 
 const Header = () => {
   return (
     <StyledHeader>
       <Logo src={logo} alt="Logo Smart Bank" />
       <div>
-        <a className="btn-secundario" href="https://google.com">
+        <BtnHeader href="https://google.com">
           Ayuda
-        </a>
-        <a className="btn-primario" href="https://google.com">
+        </BtnHeader>
+        <BtnHeader primary  href="https://google.com">
           Salir
-        </a>
+        </BtnHeader>
       </div>
     </StyledHeader>
   );
